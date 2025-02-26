ebb22865e3b641969d7ba967697ae9750bf071e0
Dlevinh <dlevinh004@gmail.com>
2025-02-26 15:47:32 +0700

    add du an quan ly sinh vien

---
+++ QuanLySinhVien/QuanLyHocSinh/Code/QuanLyTaiKhoan.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1.View
+{
+    partial class FormQuanLyTaiKhoan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.textB_oldPass = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textB_newPass = new System.Windows.Forms.TextBox();
+            this.btn_DoiMK = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(111, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(188, 24);
+            this.label6.TabIndex = 44;
+            this.label6.Text = "Quản Lý Tài Khoản";
+            // 
+            // textB_oldPass
+            // 
+            this.textB_oldPass.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textB_oldPass.Location = new System.Drawing.Point(138, 26);
+            this.textB_oldPass.Name = "textB_oldPass";
+            this.textB_oldPass.Size = new System.Drawing.Size(236, 26);
+            this.textB_oldPass.TabIndex = 47;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(22, 30);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(100, 19);
+            this.label13.TabIndex = 46;
+            this.label13.Text = "Mật Khẩu Cũ: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 19);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Mật Khẩu Mới: ";
+            // 
+            // textB_newPass
+            // 
+            this.textB_newPass.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textB_newPass.Location = new System.Drawing.Point(138, 76);
+            this.textB_newPass.Name = "textB_newPass";
+            this.textB_newPass.Size = new System.Drawing.Size(236, 26);
+            this.textB_newPass.TabIndex = 47;
+            // 
+            // btn_DoiMK
+            // 
+            this.btn_DoiMK.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btn_DoiMK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_DoiMK.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DoiMK.ForeColor = System.Drawing.Color.White;
+            this.btn_DoiMK.Location = new System.Drawing.Point(139, 142);
+            this.btn_DoiMK.Name = "btn_DoiMK";
+            this.btn_DoiMK.Size = new System.Drawing.Size(126, 37);
+            this.btn_DoiMK.TabIndex = 48;
+            this.btn_DoiMK.Text = "Đổi Mật Khẩu";
+            this.btn_DoiMK.UseVisualStyleBackColor = false;
+            this.btn_DoiMK.Click += new System.EventHandler(this.btn_DoiMK_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btn_DoiMK);
+            this.panel1.Controls.Add(this.textB_newPass);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textB_oldPass);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Location = new System.Drawing.Point(21, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(401, 199);
+            this.panel1.TabIndex = 49;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 21);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "Đổi Mật Khẩu";
+            // 
+            // FormQuanLyTaiKhoan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 352);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Name = "FormQuanLyTaiKhoan";
+            this.Text = "Quản Lý Sinh Viên";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormQuanLyTaiKhoan_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textB_oldPass;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textB_newPass;
+        private System.Windows.Forms.Button btn_DoiMK;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ QuanLySinhVien/QuanLyHocSinh/Code/TrangChu.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1.View
+{
+    public partial class FormTrangChu : Form
+    {
+        FormQuanLyTaiKhoan qltk = new FormQuanLyTaiKhoan();
+        FormQuanLyHocSinh qlhs = new FormQuanLyHocSinh();
+        FormQuanLyLop qll = new FormQuanLyLop();
+        FormQuanLyDiem qld = new FormQuanLyDiem();
+        FormQuanLyMonHoc qlmh = new FormQuanLyMonHoc();
+        FormBCTongKetMon bctkm = new FormBCTongKetMon();
+        FormThayDoiQuyDinh tdqd = new FormThayDoiQuyDinh();
+        public FormTrangChu()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        SqlConnection cnn = new SqlConnection(@"Data Source=.;Initial Catalog=QLHS;Integrated Security=True");
+
+        private void btn_QLHocSinh_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            qlhs.ShowDialog();
+            
+        }
+
+        private void btn_QLTaiKhoan_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            qltk.ShowDialog();
+        }
+
+        private void btn_QLMH_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            qlmh.ShowDialog();
+        }
+
+        private void btn_QLLop_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            qll.ShowDialog();
+        }
+
+        private void btn_QLDiem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            qld.ShowDialog();
+        }
+
+        private void btn_BCMon_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            bctkm.ShowDialog();
+        }
+
+        private void btn_TDQD_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            tdqd.ShowDialog();
+        }
+
+        private void btn_DsLop_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            string sql = "Select l.MaLop,l.KhoiLop,l.TenLop, (count(MaHS))as SiSo from Lop l, HocSinh hs where l.MaLop = hs.MaLop group by l.MaLop,l.KhoiLop,l.TenLop";  // lay het du lieu trong bang sinh vien
+            SqlCommand com = new SqlCommand(sql, cnn);
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);  
+            cnn.Close();  
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btn_DSMH_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            string sql = "Select * from MonHoc";
+            SqlCommand com = new SqlCommand(sql, cnn);
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cnn.Close();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btn_DSHS_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            string sql = "select * from HocSinh "; 
+            SqlCommand com = new SqlCommand(sql, cnn); 
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable(); 
+            da.Fill(dt);  
+            cnn.Close(); 
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btn_BCM_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            string sql = "Select * from BCTongKetMon ";
+            SqlCommand com = new SqlCommand(sql, cnn);
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cnn.Close();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void FormTrangChu_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
